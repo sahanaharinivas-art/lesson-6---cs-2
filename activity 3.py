84aaff651daf912e095a1a9d850fe15d63d2eefe
harinivassahana-max <harinivassahana@gmail.com>
2025-10-24 16:44:54 +1100

    activity 3

---
+++ activity 3.py
@@ -0,0 +1,16 @@
+BMI = wieght / (height/100)**2
+
+print("your BMI is", BMI)
+
+if BMI <= 18.4:
+    print("you are underweight.")
+elif BMI  <= 24.9:
+    print("you are healthy.")
+elif BMI <= 29.9:
+    print("you are over weight.")
+elif BMI <=34.9:
+    print(" you are severely over weight")
+elif BMI <= 39.9:
+    print("you are obese.")
+else:
+    print("you are severely obese.")
