84ac37c08628e7283a7767cc48309f807ab882c1
harinivassahana-max <harinivassahana@gmail.com>
2025-10-25 13:08:52 +1100

    assignment 6

---
+++ assignment 6.py
@@ -0,0 +1,6 @@
+ch = input("enter any character: ")
+
+if ( ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z'):
+    print("the character is an alpahbet.")
+else:
+    print("the character is not an alphabet.")
